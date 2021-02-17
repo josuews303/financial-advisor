7ee031f08a19bd2c2e3a528cb6a75a91c6aeced6
Josué Jaramillo <josuews303@hotmail.com>
2021-02-16 19:03:03 -0500

    added redux and react-foundation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from "./actions";
+import { Grid, Cell, Row, Column } from 'react-foundation';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Counter: {counter}
+      </h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button>-</button>
+      {
+        isLogged && <h3>Logged info</h3>
+      }
+
+      <Grid className="display" centerAlign>
+        <Cell small={8}>4 columns</Cell>
+      </Grid>
+
     </div>
   );
 }
